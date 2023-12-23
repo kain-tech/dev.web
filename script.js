c6123c7bb4e20c74648aac19e11895b3b2cefc07
Kaio Henrique <84056507+kain-tech@users.noreply.github.com>
2023-12-23 10:13:40 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const botaoLista = document.getElementById("botao_lista");
-
-function abrirCapitulos(){
-   const menu = document.getElementById("menu");
-
-    menu.classList.toggle("escondido");
-}
