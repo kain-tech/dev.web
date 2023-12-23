326adcc1a7110eb4037c2c019b4e71ba2cc079a4
Kaio Henrique <84056507+kain-tech@users.noreply.github.com>
2023-12-23 09:56:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const botaoLista = document.getElementById("botao_lista");
 function abrirCapitulos(){
    const menu = document.getElementById("menu");
 
-    menu.classList.toggle("visivel");
+    menu.classList.toggle("escondido");
 }
